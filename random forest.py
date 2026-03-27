44e9e2324cb651fc7602bb18f9847a151e5d938a
karthik-nd <163957940+13karthik@users.noreply.github.com>
2026-03-27 09:19:33 +0530

    Include CER and WER evaluation metrics
    Added evaluation metrics for CER and WER.

--- random forest.py
+++ random forest.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# 📂 DATASET PATH
+#  DATASET PATH
 
 DATA_PATH = r"C:\Users\karth\Desktop\data"
 
